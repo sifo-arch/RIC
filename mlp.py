1b57887d535f95b10063ada629b802ba7dc103f7
sifo-arch <smartechsbtdz@gmail.com>
2025-03-15 11:18:26 +0100

    Organize, optimize, and test the code + add the method to calculate derivatives

--- mlp.py
+++ mlp.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-"""
-A Multi Layer perceptron For Binary Classification (signle output neuron)
-"""
+
 class BinNeuralNetwork:
+    """
+    A Multi Layer perceptron For Binary Classification (signle output neuron).
+    It implements Xavier initialization to intialize weights.
+    """
     def __init__(self, hidden_layers: np.ndarray, X: np.ndarray, y: np.ndarray):
         """
         PARAMETERS:
@@ -21,7 +23,7 @@ class BinNeuralNetwork:
         # store the number of layers
         self._number_of_layers = layers.size
 
-        # the following code generates weights of each layer randomly according to Xavier technique which depends on gaussian distribution
+        # the following code generates weights of each layer randomly according to Xavier initialization which depends on gaussian distribution
         # weights are stored in a list of arrays, each element in the index i represents a 2d array of wights between layer i and i+1
         
         weights = []
@@ -54,34 +56,154 @@ class BinNeuralNetwork:
         #     print(b)
     
 
-    # the name of methods below is enoughly expressive
     def _sigmoid(self, x):
+        """
+        sigmoid as activation function
+        """
         return 1 / (1 + np.exp(-x))
+    
 
-    def forward_propagation(self):
+    def forward_propagation(self, X):
+        """
+        This method performs forward propagation on the X argument.
+        X could be one datapoint or a set of datapoints.
+        """
+        # retrieve the weights and biases of the first layer
+        W = self._weights[0]
+        b = self._biases[0]
+        # if X is a vector (single datapoint) make b a line-vector (for dimensions compatibality)
+        if X.ndim == 1:
+            b = b.flatten()
+        # calculate potential and activation of the first layer
+        potential = W.dot(X.T) + b
+        Z = self._sigmoid(potential)
+        # tracing...
+        print(f"Activations of layer[{1}]:")
+        print(Z)
+        # repeat the process for the rest of layers layers
+        for i in range(1, self._number_of_layers):
+
+            W = self._weights[i]
+            b = self._biases[i]
+
+            if X.ndim == 1:
+                b = b.flatten()
+            
+            potential = W.dot(Z) + b
+            Z = self._sigmoid(potential)
+            
+            # tracing
+            print(f"Activations of layer[{i + 1}]:")
+            print(Z)
+        
+        # return the output of the last layer (output layer)
+        return Z
+    
+    
+    def forward_outputs(self):
+        """
+        This method performs forward propagation on the entire dataset.
+        However, it saves the output of each layer in a list and returns
+        the list.
+        """
+        outputs = []
+
         X = self._X
         W = self._weights[0]
         b = self._biases[0]
         potential = W.dot(X.T) + b
         Z = self._sigmoid(potential)
+
+        outputs.append(Z)
+
         for i in range(1, self._number_of_layers):
             W = self._weights[i]
             b = self._biases[i]
             potential = W.dot(Z) + b
             Z = self._sigmoid(potential)
-        return Z
+
+            outputs.append(Z)
+        
+        return outputs
+    
+
+    def calculate_derivatives(self, outputs):
+        """
+        This method calculates derivatives of each layer.
+        outputs: a list, each element in index i is the output of layer i+1
+        """
+        # A list to store derivatives
+        derivatives = []
+        
+        # At First: Calculate the Derivative of the Last Layer
+        # last output (transpose it here to optimize the code)
+        loT = outputs[-1].T
+
+        # adjust dimensions of y
+        y = self._y.reshape((self._y.size, 1))
+        # last layer derivative
+        lld = loT * (1 - loT) * (y - loT)
+
+        # push lld to the list
+        derivatives.insert(0, lld)
+
+        # Next: Calculate the rest of layers' derivatives
+        n = self._number_of_layers
+        for i in range(n - 2, -1, -1):
+            # output of layer i
+            Oi = outputs[i]
+            # derivative of the next layer
+            nd = derivatives[0]
+            # weights between layer i and i+1
+            W = self._weights[i + 1]
+            # calculate the derivative
+            d = (Oi * (1 - Oi)).T * (nd.dot(W))
+
+            # push d to the list
+            derivatives.insert(0, d)
+        
+        return derivatives
 
 
 
 
+
+# Dataset
 X = np.array([
     [0, 0],
     [0, 1],
     [1, 0],
     [1, 1]
 ])
+# Labels
 y = np.array([0, 1, 1, 0])
-layers = np.array([5, 5])
+# Architecture
+layers = np.array([3, 2])
+# model instantiation
 model = BinNeuralNetwork(layers, X, y)
-z = model.forward_propagation()
-print(z)
+
+
+# # test forward propagation on the dataset
+# print("Forward propagation of the dataset:")
+# z_X = model.forward_propagation(X)
+
+# # test forward propagation on a new datapoint
+# xi = np.array([1, 0])
+# print(f"Forward propagation of {xi}:")
+# z_xi = model.forward_propagation(xi)
+
+# test forward_outputs
+print("Forward outputs:")
+outputs = model.forward_outputs()
+for o in outputs:
+    print(o)
+    print()
+
+# test calculate_derivatives
+print("Derivatives:")
+derivatives = model.calculate_derivatives(outputs)
+for d in derivatives:
+    print(d)
+    print()
+
+
